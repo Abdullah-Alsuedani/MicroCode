bc8f6ff11d2a1721bb57b4b5ce4f3df53471a3c1
Abdullah-Alsuedani <abdullah.xvx.2006@gmail.com>
2022-07-17 22:00:49 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 let bar = document.querySelector(".bar");
 let xmark = document.querySelector(".xmark");
 let nav = document.querySelector(".nav");
+var navLinks = document.querySelector(".nav li a");
 
+function closeNav() {
+    if (bar.classList.contains("remove")) {
+        nav.style.width="0";
+        bar.classList.remove("remove");
+        xmark.classList.remove("show");
+    }
+}
 function showNav() {
-    nav.style.height="260px";
+    nav.style.width="100%";
     bar.classList.add("remove");
     xmark.classList.add("show");
 }
 function removeNav() {
-    nav.style.height="0";
+    nav.style.width="0";
     bar.classList.remove("remove")
     xmark.classList.remove("show");
 }
