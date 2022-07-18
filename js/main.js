823283456eaa614b929e8955e0190a5a6fd27dc2
Abdullah-Alsuedani <abdullah.xvx.2006@gmail.com>
2022-07-18 18:39:12 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -21,18 +21,28 @@ function removeNav() {
 }
 
 // Scroll Animations
+let aboutTitle  = document.querySelector(".about .title");
+let aboutContent = document.querySelectorAll(".about p");
+
 let servicesSection = document.querySelector(".services");
 let servicesRow = document.querySelectorAll(".services .row");
 let servicesText = document.querySelectorAll(".services .text");
 let servicesImg = document.querySelectorAll(".services img");
-
-let aboutTitle  = document.querySelector(".about .title");
-let aboutContent = document.querySelectorAll(".about p")
-
 let ServicesTitle  = document.querySelector(".services .title");
 
+let clientsTitle = document.querySelector(".clients .title");
+let companies = document.querySelector(".clients .companies");
+let stats = document.querySelector(".clients .stats");
 
 window.onscroll = function () {
+    if (window.scrollY >= aboutTitle.offsetParent.offsetTop - 500) {
+        aboutTitle.style.animation="running";
+    }
+    if (window.scrollY >= aboutContent[0].offsetParent.offsetTop - 500) {
+        aboutContent.forEach((Content) => {
+            Content.style.animation="running";
+        });
+    }
     if (window.scrollY >= (servicesSection.offsetTop + servicesRow[0].offsetTop -500)) {
         servicesText[0].style.animation = "running";
         servicesImg[0].style.animation = "running";
@@ -49,12 +59,32 @@ window.onscroll = function () {
         servicesText[3].style.animation = "running";
         servicesImg[3].style.animation = "running";
     }
-    if (window.scrollY >= aboutTitle.offsetParent.offsetTop - 500) {
-        aboutTitle.style.animation="running";
+    if (window.scrollY >= ServicesTitle.offsetParent.offsetTop - 500) {
+        ServicesTitle.style.animation="running";
     }
-    if (window.scrollY >= aboutContent[0].offsetParent.offsetTop - 500) {
-        aboutContent.forEach((Content) => {
-            Content.style.animation="running";
-        });
+    if (window.scrollY >= clientsTitle.offsetParent.offsetTop - 500) {
+        clientsTitle.style.animation="running";
+    }
+    if (window.scrollY >= companies.offsetParent.offsetTop - 500) {
+        companies.style.animation="running";
+    }
+    if (window.scrollY >= stats.offsetParent.offsetTop - 500) {
+        stats.style.animation="running";
     }
 };
+
+// Stats Increase Number
+
+let nums = document.querySelectorAll(".stats h3 span");
+
+nums.forEach((num) => {startCount(num)})
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent = goal) {
+            clearInterval(count);
+        }
+    }, 20)
+}
