a0d265cf5ccd2ddf16599a989515338979c52176
Abdullah-Alsuedani <abdullah.xvx.2006@gmail.com>
2022-07-18 15:33:47 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let bar = document.querySelector(".bar");
 let xmark = document.querySelector(".xmark");
 let nav = document.querySelector(".nav");
-var navLinks = document.querySelector(".nav li a");
 
 function closeNav() {
     if (bar.classList.contains("remove")) {
@@ -20,3 +19,42 @@ function removeNav() {
     bar.classList.remove("remove")
     xmark.classList.remove("show");
 }
+
+// Scroll Animations
+let servicesSection = document.querySelector(".services");
+let servicesRow = document.querySelectorAll(".services .row");
+let servicesText = document.querySelectorAll(".services .text");
+let servicesImg = document.querySelectorAll(".services img");
+
+let aboutTitle  = document.querySelector(".about .title");
+let aboutContent = document.querySelectorAll(".about p")
+
+let ServicesTitle  = document.querySelector(".services .title");
+
+
+window.onscroll = function () {
+    if (window.scrollY >= (servicesSection.offsetTop + servicesRow[0].offsetTop -500)) {
+        servicesText[0].style.animation = "running";
+        servicesImg[0].style.animation = "running";
+    }
+    if (window.scrollY >= (servicesSection.offsetTop + servicesRow[1].offsetTop -500)) {
+        servicesText[1].style.animation = "running";
+        servicesImg[1].style.animation = "running";
+    }
+    if (window.scrollY >= (servicesSection.offsetTop + servicesRow[2].offsetTop -500)) {
+        servicesText[2].style.animation = "running";
+        servicesImg[2].style.animation = "running";
+    }
+    if (window.scrollY >= (servicesSection.offsetTop + servicesRow[3].offsetTop -500)) {
+        servicesText[3].style.animation = "running";
+        servicesImg[3].style.animation = "running";
+    }
+    if (window.scrollY >= aboutTitle.offsetParent.offsetTop - 500) {
+        aboutTitle.style.animation="running";
+    }
+    if (window.scrollY >= aboutContent[0].offsetParent.offsetTop - 500) {
+        aboutContent.forEach((Content) => {
+            Content.style.animation="running";
+        });
+    }
+};
